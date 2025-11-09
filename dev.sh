6fc309c6aa5753285ed373ebdfbcae67685411d2
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-11-09 22:31:49 +0700

    chore: add delay to dev bash

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 cmake --build build
 adb push build/lib*.so /storage/emulated/0
 adb shell am start -n git.artdeell.skymodloader/.elfmod.ModManagerActivity
+sleep 1
 adb logcat --pid=$(adb shell pidof -s git.artdeell.skymodloader) -v color | grep RobinHood

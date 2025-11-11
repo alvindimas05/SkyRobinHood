82dfc1eeb3ad86b871f8ec1d9da12c1e7a8dc60a
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-11-11 17:05:16 +0700

    fix: typo DuskStart

--- src/model/map/map.cpp
+++ src/model/map/map.cpp
@@ -6,7 +6,7 @@ static const std::vector<std::string> maps = {
     "Prairie_ButterflyFields", "Prairie_Cave", "Prairie_NestAndKeeper", "Prairie_Island", "Prairie_Village", "Prairie_WildLifePark", "DayHubCave", "DayEnd",
     "Rain", "RainForest", "RainMid", "RainShelter", "Rain_Cave", "Rain_BaseCamp", "RainEnd", "Skyway",
     "Sunset", "SunsetRace", "Sunset_Citadel", "Sunset_FlyRace", "SunsetColosseum", "SunsetEnd2", "SunsetVillage", "Sunset_YetiPark",
-    "Dusk_Start", "Dusk", "Dusk_Triangle", "DuskOasis", "DuskGraveyard", "Dusk_CrabField", "DuskMid", "DuskEnd",
+    "DuskStart", "Dusk", "Dusk_Triangle", "DuskOasis", "DuskGraveyard", "Dusk_CrabField", "DuskMid", "DuskEnd",
     "Night", "Night2", "NightEnd", "NightArchive", "Night_Shelter", "Night_IPHallway",
     "NightDesert", "NightDesert_Beach", "Night_JarCave", "Night_PaintedWorld",
     "Event_DaysOfMischief", "TGCOffice"};
